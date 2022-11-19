62a0d1cc9a76e7c56e7f6d8cb08b8a2089a8e3cb
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2022-11-19 16:10:26 +0700

    Finished Xylophone App development
    Finally, finished this Xylophone App development from Angela Yu App Brewery's iOS course
    
    This app now run perfectly as it should be and designed in mind for iPhone 11. I might be added Auto Layout and Responsive UI to this app (also to the Dicee and Magic 8 Ball app) so i can sideload these app to test it natively on my iPhone SE (2020).

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -3,19 +3,43 @@
 //  Xylophone
 //
 //  Created by Angela Yu on 28/06/2019.
+//  Studied by Putra Ganda Dewata
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        //print("I got pressed") // Debug
+        //print(sender.currentTitle) // Debug
+        
+        playSound(soundName: sender.currentTitle!)
+        print("Start") // printed immediately
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
 
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+            print("End") // printed after the button pressed + 0.2 seconds delay
+        }
+        
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
